954d1a4f017486ecdc054e7819a876e843e3125d
serdaraltin <meyta.tr@gmail.com>
2021-06-18 22:34:10 +0300

    upload

---
+++ Downloader/downloader.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+namespace ToolBox
+{
+    public partial class downloader : Form
+    {
+        WebClient wc = new WebClient();
+        public downloader()
+        {
+            InitializeComponent();
+        }
+        int Move;
+        int Move_x;
+        int Move_y;
+        private void downloader_Load(object sender, EventArgs e)
+        {
+            textBox3.Text=@"C:\Users\"+Environment.UserName+"\\Downloads";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                comboBox1.Enabled = false;
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button6.Enabled = true;
+                wc.DownloadFileCompleted += new AsyncCompletedEventHandler(filedownloadcoplate);
+                Uri fileurl = new Uri(textBox1.Text);
+                wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressCallback);
+                wc.DownloadFileAsync(fileurl, textBox3.Text+"\\"+textBox2.Text+"."+comboBox1.Text);
+            }
+            catch (Exception hata)
+            {
+                MessageBox.Show(hata.Message.ToString() + "\nİndirilebilir bir dosya değil", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                comboBox1.Enabled = true;
+                button6.Enabled = false;
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+          
+        }
+     
+        private void filedownloadcoplate(object sender, AsyncCompletedEventArgs e)
+        {
+           // MessageBox.Show("İndirme tamamlandı.","Başarılı",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            button3.Enabled = true;
+            button4.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            button6.Enabled = false;
+            button1.Enabled = true;
+            
+        }
+        private  void DownloadProgressCallback(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double klobyte =Convert.ToDouble(e.BytesReceived) / 1024;
+            double klobyte2 = Convert.ToDouble(e.TotalBytesToReceive) / 1024;
+            /*int klbyte = Convert.ToInt32(klobyte);
+            int klbyte2 = Convert.ToInt32(klobyte2);*/
+            string indirilen = klobyte.ToString("N0");
+            string tamboyut = klobyte2.ToString("N0");
+
+
+            if (tamboyut.Length < 4)
+            {
+                label2.Text = indirilen + " / " + tamboyut + " Kbyte.  %" + e.ProgressPercentage.ToString() + " tamamlandı...";
+                progressBar1.Value = e.ProgressPercentage;
+            }
+
+            else if (tamboyut.Length > 4 && tamboyut.Length < 7)
+            {
+                tamboyut= tamboyut.Remove(1,4);
+                
+                label2.Text = indirilen + " / " + tamboyut + " Mbyte.  %" + e.ProgressPercentage.ToString() + " tamamlandı...";
+                progressBar1.Value = e.ProgressPercentage;
+            }
+            else if (tamboyut.Length >= 7)
+            {
+                tamboyut = tamboyut.Remove(3, 6);
+                label2.Text = indirilen + " / " + tamboyut + " Gbyte.  %" + e.ProgressPercentage.ToString() + " tamamlandı...";
+                progressBar1.Value = e.ProgressPercentage;
+            }
+          
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog kayıtyeri = new FolderBrowserDialog();
+            if (kayıtyeri.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = kayıtyeri.SelectedPath;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text != "")
+                {
+                    System.Diagnostics.Process.Start(textBox3.Text);
+                }
+                else
+                {
+                    button4.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox2.Text != "")
+                {
+                    System.Diagnostics.Process.Start(textBox3.Text + "\\" + textBox2.Text + "." + comboBox1.Text);
+                }
+                else
+                {
+                    button3.Enabled = false;
+                }
+            }
+            catch { }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            wc.CancelAsync();
+            button3.Enabled = true;
+            button4.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            button6.Enabled = false;
+            button1.Enabled = true;
+            textBox2.Text = "";
+            System.IO.File.Delete(textBox3.Text + "\\" + textBox2.Text + "." + comboBox1.Text);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled == false)
+            {
+                notifyIcon1.Visible = true;
+                this.Visible = false;
+            }
+        }
+
+        private void indirmeyiGösterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            this.Visible = true;
+        }
+
+        private void downloader_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           
+        }
+
+        private void downloader_FormClosed(object sender, FormClosedEventArgs e)
+        {
+          
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            DialogResult indirme = MessageBox.Show("Kapatmak istediğinize emin misiniz?", "Dikkat", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (indirme == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void downloader_MouseUp(object sender, MouseEventArgs e)
+        {
+            Move = 0;
+        }
+
+        private void downloader_MouseDown(object sender, MouseEventArgs e)
+        {
+            Move = 1;
+            Move_x = e.X;
+            Move_y = e.Y;
+        }
+
+        private void downloader_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Move == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - Move_x, MousePosition.Y - Move_y);
+            }
+        }
+
+        private void iptalEtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            wc.CancelAsync();
+            button3.Enabled = true;
+            button4.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            textBox3.Enabled = true;
+            button6.Enabled = false;
+            button1.Enabled = true;
+            textBox2.Text = "";
+            System.IO.File.Delete(textBox3.Text + "\\" + textBox2.Text + "." + comboBox1.Text);
+        }
+
+       
+    }
+}
---
+++ Downloader/downloader.designer.cs
@@ -0,0 +1,356 @@
+﻿namespace ToolBox
+{
+    partial class downloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(downloader));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.indirmeyiGösterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iptalEtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "İndirmeyi Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Url";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(11, 148);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(350, 22);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(8, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "%";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "jpg",
+            "png",
+            "gif",
+            "exe",
+            "txt",
+            "vbs",
+            "php",
+            "html",
+            "iso",
+            "rar",
+            "doc"});
+            this.comboBox1.Location = new System.Drawing.Point(59, 116);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 23);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Text = "rar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Format";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "İsim";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(301, 23);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "download";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Konum";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(60, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(268, 23);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(11, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 25);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Aç";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(106, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(89, 25);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Klasör Aç";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(272, 197);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 25);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Kapat";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(272, 115);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(89, 25);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Durdur";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(257, 2);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(32, 20);
+            this.button7.TabIndex = 16;
+            this.button7.Text = ">>";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.indirmeyiGösterToolStripMenuItem,
+            this.iptalEtToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(162, 48);
+            // 
+            // indirmeyiGösterToolStripMenuItem
+            // 
+            this.indirmeyiGösterToolStripMenuItem.Name = "indirmeyiGösterToolStripMenuItem";
+            this.indirmeyiGösterToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.indirmeyiGösterToolStripMenuItem.Text = "İndirmeyi Göster";
+            this.indirmeyiGösterToolStripMenuItem.Click += new System.EventHandler(this.indirmeyiGösterToolStripMenuItem_Click);
+            // 
+            // iptalEtToolStripMenuItem
+            // 
+            this.iptalEtToolStripMenuItem.Name = "iptalEtToolStripMenuItem";
+            this.iptalEtToolStripMenuItem.Size = new System.Drawing.Size(161, 22);
+            this.iptalEtToolStripMenuItem.Text = "İptal Et";
+            this.iptalEtToolStripMenuItem.Click += new System.EventHandler(this.iptalEtToolStripMenuItem_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button8.Location = new System.Drawing.Point(295, 2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(32, 20);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button9.Location = new System.Drawing.Point(329, 2);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(32, 20);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "X";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // downloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(370, 228);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "downloader";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Downloader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.downloader_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.downloader_FormClosed);
+            this.Load += new System.EventHandler(this.downloader_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.downloader_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.downloader_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.downloader_MouseUp);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem indirmeyiGösterToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iptalEtToolStripMenuItem;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
